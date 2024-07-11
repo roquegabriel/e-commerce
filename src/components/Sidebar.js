d4322f82cf5df89873cd2a415ad880fa3928d47e
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-07-11 16:18:53 -0400

    Update sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,18 +7,18 @@ import { FiTrash2 } from 'react-icons/fi'
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext)
-  const { cart, clearCart} = useContext(CartContext)
+  const { cart, clearCart,itemAmount,total} = useContext(CartContext)
   return (
     <div className={`${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
       <div className='flex justify-between items-center py-6 border-b'>
         <div className='uppercase text-sm font-semibold'>
-          Shopping Bag (0)
+          Shopping Bag ({itemAmount})
         </div>
         <div onClick={handleClose} className='cursor-pointer w-8 h-8 flex justify-center items-center'>
           <IoMdArrowForward className='text-2xl' />
         </div>
       </div>
-      <div>
+      <div className='flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b'>
         {
           cart.map((item) => {
             return <CartItem item={item} />
@@ -28,7 +28,7 @@ const Sidebar = () => {
       <div className='flex flex-col gap-y-3 py-4 mt-4'>
         <div className='flex justify-between items-center w-full'>
           {/* total */}
-          <div className='uppercase font-semibold'><span className='mr-2'>Total:</span>$ 1000
+          <div className='uppercase font-semibold'><span className='mr-2'>Total:</span>$ {parseFloat(total).toFixed(2)}
         </div>
         {/* clear cart button */}
         <div className='cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center items-center text-xl' onClick={clearCart}>
