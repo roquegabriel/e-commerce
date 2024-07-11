ad27608b127db2a5784d3f0570d15da581437926
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-07-11 16:22:36 -0400

    Include add to cart button

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,8 +3,10 @@ import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 //import icons
 import { BsPlus, BsEyeFill } from 'react-icons/bs'
+import { CartContext } from '../contexts/CartContext'
 
 const Product = ({ product }) => {
+  const {addToCart} = useContext(CartContext)
   //Desctructure product
   const { id, image, category, title, price } = product
 
@@ -18,7 +20,7 @@ const Product = ({ product }) => {
           </div>
           {/* buttons */}
           <div className='absolute top-6 -right-11 group-hover:right-5 bg-red-500/40 p-2 flex flex-col items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300'>
-            <button>
+            <button onClick={() => addToCart(id,product)}>
               <div className='flex justify-center items-center text-white w-12 h-12 bg-red-500'>
                 <BsPlus className='text-3xl' />
               </div>
