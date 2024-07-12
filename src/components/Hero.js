c130bc1ba806baaa0bf64633cafcb9390014d7b7
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-07-11 21:13:41 -0400

    Add hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import BackgroundHero from '../assets/images/background-hero.jpeg'
+import WomanHero from '../assets/images/hero.png'
+import { Link } from 'react-router-dom'
+
+
+const Hero = () => {
+  return (
+    <section className='bg-sky-50 mt-20'>
+      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2">
+        {/* text */}
+        <div className='flex flex-col justify-center'>
+          {/* pretitle */}
+          <div className='font-semibold flex items-center'>
+            <div className='w-10 bg-red-500 h-[2px] mr-3'></div>New Trend
+          </div>
+          {/* title */}
+          <h1 className='text-[70px] leading-[1.1]  font-light mb-4'>Find your true style here</h1>
+          <Link to={'/'} className='uppercase font-semibold border-b-2 border-primary self-start'>
+          Discover more
+          </Link>
+        </div>
+        {/* image */}
+        <div className='hidden lg:block'>
+          <img src={WomanHero} alt="" className='' />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
