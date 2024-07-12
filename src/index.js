6d5d103b211729740553a96bd0900d0abfffae6d
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-07-11 21:11:01 -0400

    Modify index

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+//import product provider
+import ProductProvider from './contexts/ProductContext';
+//import cart provider
+import CartProvider from './contexts/CartContext';
+// import sidebar provider
+import SidebarProvider from './contexts/SidebarContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <SidebarProvider>
+    <CartProvider>
+    <ProductProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ProductProvider>
+    </CartProvider>
+  </SidebarProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
